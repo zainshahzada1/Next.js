044a8d83c0f9f7ff8f7c071cde103fab2b2c2001
Asad Raza <asad.9800545@talmeez.pk>
2022-04-19 14:19:22 +0500

    first commit

---
+++ nextjsapp/pages/about.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function about() {
+  return (
+    <div>Hello I am About page</div>
+  )
+}
+
+export default about
---
+++ nextjsapp/pages/blogposts/using_keyboard.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const how = () => {
+  return (
+    <div>This is Keyboard Page</div>
+  )
+}
+
+export default how
